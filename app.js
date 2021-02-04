2798310328e3d16e54a6039e6d7ae6cea4327b28
Moshfegh Hamedani <moshfegh@live.com.au>
2021-02-04 10:44:11 -0800

    Update message

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // db.on('error', console.log);
 
 app.get('/', (req, res) => {
-  res.send('<h1>App is running!</h1>');
+  res.send('<h1>App is running!!!!</h1>');
 });
 
 app.get('/movies', async (req, res) => {
